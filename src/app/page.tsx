7d95517cd703da3203bf4f535c0721f08a7e43ad
mariannebourcier <marianne.bourcier@gmail.com>
2023-11-02 15:24:53 -0600

    remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       hi
-       <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+      
       </div>
     </main>
   )
