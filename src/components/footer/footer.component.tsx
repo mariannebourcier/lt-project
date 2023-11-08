90b0039e6bbae4e859bb54a60d0bb022655d4a75
mariannebourcier <marianne.bourcier@gmail.com>
2023-11-07 21:50:34 -0700

    style for desktop

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,21 +1,27 @@
+'use client'
 import React from 'react'
 import style from './footer.module.scss'
+import { useRouter } from 'next/navigation'
 
 const Footer = () => {
+  const router = useRouter();
   return (
     <div className={style.main_container}>
       <div className={style.sub_container}>
-        <span>Home</span>
+      <div className={style.content_container}>
+        <span onClick={() => router.push('/')}>Home</span>
         <span>Planning</span>
         <span>Resources</span>
         <span>Contact Us</span>
         <span>Register / Login</span>
       </div>
-      <div className={style.sub_container}>
+      <div className={style.content_container}>
         <span>About</span>
         <span>Our team</span>
         <span>Security & Privacy</span>
       </div>
+      </div>
+     
     <div className={style.logo_container}>
       🐁
     </div>
